7e6784e3f1168fdd189454f43b1f65ee2cc4d14b
NdikRP <andhikarizky36@gmail.com>
2023-09-09 12:51:00 +0700

    third commit

--- fitur.js
+++ fitur.js
@@ -2,16 +2,12 @@ document.getElementById('navbarToggler').addEventListener('click', function () {
     var menu = document.getElementById('menu');
     var toggler = this;
 
-    // Tambahkan transisi opacity ke menu
     menu.style.transition = 'opacity 0.3s ease-in-out';
 
-    // Toggle class "active" pada menu
     menu.classList.toggle('active');
 
-    // Toggle class "active" pada toggler
     toggler.classList.toggle('active');
 
-    // Atur opacity menu berdasarkan class "active"
     if (menu.classList.contains('active')) {
         menu.style.opacity = '1';
     } else {
@@ -20,10 +16,7 @@ document.getElementById('navbarToggler').addEventListener('click', function () {
 });
 
 document.getElementById('connectButton').addEventListener('click', function () {
-    // Ganti URL berikut dengan tautan profil LinkedIn Anda
     var linkedinProfileURL = 'https://www.linkedin.com/in/andhika-rizky/';
-
-    // Buka tautan LinkedIn dalam jendela/tab baru
     window.open(linkedinProfileURL, '_blank');
 });
 
